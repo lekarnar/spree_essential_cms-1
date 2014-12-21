d8b4cb314e7e92a3611a32cd6e9fbb1caec5859b
Waylon Peele <waylon@cloudcliques.com>
2014-12-21 14:11:12 -0600

    patch to fix upload endless loop

--- app/models/spree/page_image.rb
+++ app/models/spree/page_image.rb
@@ -11,16 +11,16 @@ class Spree::PageImage < Spree::Asset
   validates_attachment :attachment,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
-  # def image_content?
-  #   attachment_content_type.to_s.match(/\/(jpeg|png|gif|tiff|x-photoshop)/)
-  # end
+  def image_content?
+    attachment_content_type.to_s.match(/\/(jpeg|png|gif|tiff|x-photoshop)/)
+  end
 
   def attachment_sizes
     sizes = {}
-    #if image_content?
+    if image_content?
       sizes.merge!(:mini => '48x48>', :small => '150x150>', :medium => '800x800#', :large => '1000x1000#')
       sizes.merge!(:slide => '1500x') if viewable.respond_to?(:root?) && viewable.root?
-    #end
+    end
     sizes
   end
 
